3a7c0892a7202cda93dc2a632f241fced84ca57c
alex192 <16tooleya01@sch.gg>
2026-08-22 15:15:08 +0100

    Update interpreter.js
    Added functionality for comma
    Fixed ]

--- interpreter.js
+++ interpreter.js
@@ -2,13 +2,27 @@
 //  [ ]     [ ]+ ,-+.,]>   []]  ,-,,--.,    ,-++-,   ,., ,,,    .--.    [ ] ,.,
 //  [ ]+-,  [   +  ,---,   .-.  ]   ,-, ]  [  ,-,,]  ] ] [ [   [ ,-.]   ] [+ +
 //  [  [] ] [  ]  [ [] ],  [ ]  ] +   ] [  ] ,,[     ] ]-[ [   [ [,..   ] , [
-//  [ ,,, < [.]   \.,.>,]  [,]  ].[   ]_[  ],[        +..+,.]   +.,.+   [ [>,[
+//  [ ,,, < [.]   \.,.>,]  [,]  ].[   ]_[  ],[       +-,.+,.]   +.,.+   [ [>,[
 
+// Start of AI-generated code
+
+// Helper function to pause execution for a specific time
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+// Polling utility that waits for a condition function to return true
+const waitUntil = async (conditionFunction, checkInterval = 100) => {
+  while (!conditionFunction()) {
+    await delay(checkInterval); // Check every 100ms
+  }
+};
+// End of AI-generated code
+
+const CODE_BOX = getElementById("bf-code")
 const INPUT_BOX = getElementById("bf-input")
 const OUTPUT_BOX = getElementById("bf-output")
 class Tape = {
-  constructor(ptr, ...values) {
-    this.ptr = ptr;
+  constructor(...values) {
+    this.ptr = 0;
     this.values = values;
   }
   
@@ -16,12 +30,13 @@ class Tape = {
     return this.values[Symbol.iterator]();
   }
 }
-const tape = [0]
+const tape = new Tape(...[0])
 function evaluateBF(code, tape) {
   const string = code;
-  let idx = 0;
+	let comma_counter = 0;
   const sr_startpoints = []
-  for(char of string) {
+  for(let idx = 0; idx < string.length; idx++;) {
+		let char = string[idx];
     switch (char) {
       case "+":
         tape[tape.ptr] = (tape[tape.ptr] + 1) % 256;
@@ -29,6 +44,7 @@ function evaluateBF(code, tape) {
         tape[tape.ptr] = (tape[tape.ptr] - 1) % 256;
       case ">":
         tape.ptr += 1;
+				if(tape.ptr >= tape.length){tape.push(0)}
       case "<":
         tape.ptr -= 1;
       case "[":
@@ -49,6 +65,11 @@ function evaluateBF(code, tape) {
 					default:
         		OUTPUT_BOX.innerHTML += String.fromCharCode(tape[tape.ptr]);
 			case ",":
-				// TODO: add input
+				if (comma_counter >= INPUT_BOX.value.length) {INPUT_BOX.style = "color: darkred;";
+																													waitUntil(comma_counter < INPUT_BOX.innevaluerHTML.length, 250);
+																												  INPUT_BOX.style = "color: black;";};
+				tape[tape.ptr] = INPUT_BOX.value.charCodeAt(comma_counter);
+				comma_counter++;
   }
 }
+window.onload = () => {CODE_BOX.addEventListener("input", evaluateBF(CODE_BOX.value, tape))}
